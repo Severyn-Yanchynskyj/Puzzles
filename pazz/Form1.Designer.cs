aeaf381275dedc87a01802421280e3af368a010f
Северин Янчинський <yanchynskyjs@gmail.com>
2021-11-22 12:39:27 +0200

    Add project files.

---
+++ pazz/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿
+namespace pazz
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.split_button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.check_button = new System.Windows.Forms.Button();
+            this.compose_button = new System.Windows.Forms.Button();
+            this.select_button = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.composing_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(282, 229);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(120, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Picture";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(600, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Puzzles";
+            // 
+            // split_button
+            // 
+            this.split_button.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.split_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.split_button.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.split_button.Location = new System.Drawing.Point(200, 318);
+            this.split_button.MaximumSize = new System.Drawing.Size(150, 40);
+            this.split_button.MinimumSize = new System.Drawing.Size(60, 29);
+            this.split_button.Name = "split_button";
+            this.split_button.Size = new System.Drawing.Size(94, 29);
+            this.split_button.TabIndex = 4;
+            this.split_button.Text = "Split";
+            this.split_button.UseVisualStyleBackColor = false;
+            this.split_button.Click += new System.EventHandler(this.Split_button_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(51, 318);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(94, 27);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "4";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(51, 373);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(94, 27);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "4";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(506, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(282, 229);
+            this.panel1.TabIndex = 7;
+            // 
+            // check_button
+            // 
+            this.check_button.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.check_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.check_button.Location = new System.Drawing.Point(506, 318);
+            this.check_button.MaximumSize = new System.Drawing.Size(150, 40);
+            this.check_button.MinimumSize = new System.Drawing.Size(60, 29);
+            this.check_button.Name = "check_button";
+            this.check_button.Size = new System.Drawing.Size(94, 29);
+            this.check_button.TabIndex = 8;
+            this.check_button.Text = "Check";
+            this.check_button.UseVisualStyleBackColor = false;
+            this.check_button.Click += new System.EventHandler(this.Check_button_Click);
+            // 
+            // compose_button
+            // 
+            this.compose_button.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.compose_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.compose_button.Location = new System.Drawing.Point(506, 371);
+            this.compose_button.MaximumSize = new System.Drawing.Size(150, 40);
+            this.compose_button.MinimumSize = new System.Drawing.Size(60, 29);
+            this.compose_button.Name = "compose_button";
+            this.compose_button.Size = new System.Drawing.Size(94, 29);
+            this.compose_button.TabIndex = 9;
+            this.compose_button.Text = "Compose";
+            this.compose_button.UseVisualStyleBackColor = false;
+            this.compose_button.Click += new System.EventHandler(this.Compose_button_Click);
+            // 
+            // select_button
+            // 
+            this.select_button.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.select_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.select_button.Location = new System.Drawing.Point(200, 373);
+            this.select_button.MaximumSize = new System.Drawing.Size(150, 40);
+            this.select_button.MinimumSize = new System.Drawing.Size(60, 29);
+            this.select_button.Name = "select_button";
+            this.select_button.Size = new System.Drawing.Size(94, 29);
+            this.select_button.TabIndex = 10;
+            this.select_button.Text = "Select";
+            this.select_button.UseVisualStyleBackColor = false;
+            this.select_button.Click += new System.EventHandler(this.Select_button_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 318);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 28);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "X :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(12, 372);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 28);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Y :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(336, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 23);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Instructions :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(314, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(175, 115);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "*Leftclick on puzzles\r\n to swap them\r\n\r\n**Rightclick on puzzle\r\n to spin it";
+            // 
+            // label7
+            // 
+            this.composing_label.AutoSize = true;
+            this.composing_label.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.composing_label.Location = new System.Drawing.Point(314, 217);
+            this.composing_label.Name = "label7";
+            this.composing_label.Size = new System.Drawing.Size(124, 23);
+            this.composing_label.TabIndex = 15;
+            this.composing_label.Text = "Composing . . .";
+            this.composing_label.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.composing_label);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.select_button);
+            this.Controls.Add(this.compose_button);
+            this.Controls.Add(this.check_button);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.split_button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.MinimumSize = new System.Drawing.Size(700, 400);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button split_button;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button check_button;
+        private System.Windows.Forms.Button compose_button;
+        private System.Windows.Forms.Button select_button;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label composing_label;
+    }
+}
+
